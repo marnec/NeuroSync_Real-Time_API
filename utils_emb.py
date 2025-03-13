18eae77c77bc18e129b418f17cdf2d9b10966c6a
AnimaVR <22724774+AnimaVR@users.noreply.github.com>
2025-03-13 10:27:18 +0000

    Delete utils_emb.py

--- utils_emb.py
+++
@@ -1,6 +0,0 @@
-
-def process_embedding(text, embmodel):
-    if not text:
-        raise ValueError("No text data provided.")
-    embedding = embmodel.encode([text])[0]
-    return {'embedding': embedding.tolist()}
