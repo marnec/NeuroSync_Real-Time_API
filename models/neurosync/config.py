f700f350efd041374ace4480611203db890e7bc4
AnimaVR <22724774+AnimaVR@users.noreply.github.com>
2025-03-19 23:53:02 +0000

    Update config.py

--- models/neurosync/config.py
+++ models/neurosync/config.py
@@ -2,10 +2,10 @@ config = {
     'sr': 88200,
     'frame_rate': 60,
     'hidden_dim': 1024,
-    'n_layers': 8,           # Number of layers in the model
+    'n_layers': 8,         
     'num_heads': 16, 
     'dropout': 0.0,
-    'output_dim': 68,
+    'output_dim': 68, # if you trained your own, this should be 61
     'input_dim': 256,
     'frame_size': 128,
     'use_half_precision': False
